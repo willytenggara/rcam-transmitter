048d20fe82a5a60b82449d8da360a49820c7362b
willy <willybernardus@gmail.com>
2026-05-22 22:22:29 +0700

    M1: scaffold + device enumeration working

---
+++ src/listDevices.ts
@@ -0,0 +1,12 @@
+import { RtAudio, RtAudioApi } from "audify";
+
+const rtAudio = new RtAudio(RtAudioApi.WINDOWS_WASAPI);
+
+const devices = rtAudio.getDevices();
+
+devices.forEach((device, index) => {
+  console.log(`[${index}] ${device.name}`);
+  console.log(`     inputs:  ${device.inputChannels}`);
+  console.log(`     outputs: ${device.outputChannels}`);
+  console.log("---");
+});
